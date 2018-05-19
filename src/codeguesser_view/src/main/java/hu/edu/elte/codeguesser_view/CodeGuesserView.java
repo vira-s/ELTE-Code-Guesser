8bc73079ffbca9d2b50182dae77d864417bae793
Emese András <8emi95@inf.elte.hu>
2018-05-19 20:36:45 +0200

    fixed game panel

--- src/codeguesser_view/src/main/java/hu/edu/elte/codeguesser_view/CodeGuesserView.java
+++ src/codeguesser_view/src/main/java/hu/edu/elte/codeguesser_view/CodeGuesserView.java
@@ -7,12 +7,11 @@ import hu.edu.elte.codeguesser_view.listeners.CustomActionListener;
 import hu.edu.elte.codeguesser_view.listeners.CustomWindowListener;
 import hu.edu.elte.codeguesser_view.menu.GameMenu;
 
-import javax.swing.AbstractAction;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import java.awt.GridLayout;
+import javax.swing.*;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author virabia on 5/10/2018
@@ -25,9 +24,17 @@ public class CodeGuesserView extends JFrame {
     private CustomWindowListener windowListener;
     private JPanel panel;
 
-    private GameButton exitButton;
+    private JLabel gameLevel;
+    private JLabel codeLength;
+    private JLabel maxGuesses;
+    private JTextArea previousGuesses;
+    private JTextField guessNumbers;
     private SubmitButton submitButton;
-    private GameButton newGameButton;
+    private JLabel remainingGuesses;
+    private GameButton giveUpButton;
+
+    // private GameButton exitButton;
+    // private GameButton newGameButton;
 
     public CodeGuesserView(CustomActionListener actionListener) {
         super(WINDOW_TITLE);
@@ -45,23 +52,76 @@ public class CodeGuesserView extends JFrame {
     }
 
     private void initializeWindow() {
-        setSize(800, 600);
+        setSize(1000, 700);
         addWindowListener(windowListener);
 
         setJMenuBar(new GameMenu(this));
 
-        JLabel label = new JLabel("333333");
+        panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
+
+        gameLevel = new JLabel("Level: " + "getGameLevel()");
+        codeLength = new JLabel("Digits: " + "getCodeLength()");
+        maxGuesses = new JLabel("Maximum Guesses: " + "getMaxGuesses()");
+
+        previousGuesses = new JTextArea("--------------------------------------------Previous Guesses:--------------------------------------------\n");
+        previousGuesses.setEditable(false);
+        previousGuesses.setRows(26);
+
+        guessNumbers = new JTextField(10);
+        // submitButton = new SubmitButton(ActionType.SUBMIT, actionListener);
+        JButton submitButton_TEMPORARY = new JButton("Submit");
+        submitButton_TEMPORARY.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                previousGuesses.append("getGuess()" + " - correct: " + "getCorrect()" + ", contains: " + "getContaining()" + ", wrong: " + "getWrong()\n");
+            }
+        });
+
+        remainingGuesses = new JLabel("Remaining guesses: " + "getRemainingGuesses()");
+        giveUpButton = new GameButton(ActionType.GIVE_UP, actionListener);
+
+        JLabel label = new JLabel("Submit");
         initializeSubmitButton(label);
 
-        newGameButton = new GameButton(ActionType.NEW_GAME_MEDIUM, actionListener);
-        exitButton = new GameButton(ActionType.EXIT, actionListener);
+        gc.weightx = 0.5;
+        gc.weighty = 0.5;
 
-        panel = new JPanel();
-        panel.setLayout(new GridLayout(1, 1));
-        panel.add(exitButton);
-        panel.add(submitButton);
-        panel.add(newGameButton);
-        panel.add(label);
+        gc.gridx = 0;
+        gc.gridy = 0;
+        panel.add(gameLevel, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 0;
+        panel.add(codeLength, gc);
+
+        gc.gridx = 2;
+        gc.gridy = 0;
+        panel.add(maxGuesses, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 1;
+        panel.add(previousGuesses, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 2;
+        panel.add(guessNumbers, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 3;
+        panel.add(submitButton_TEMPORARY, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 4;
+        panel.add(remainingGuesses, gc);
+
+        gc.gridx = 2;
+        gc.gridy = 5;
+        panel.add(giveUpButton, gc);
+
+        // newGameButton = new GameButton(ActionType.NEW_GAME_MEDIUM, actionListener);
+        // exitButton = new GameButton(ActionType.EXIT, actionListener);
 
         this.add(panel);
     }
